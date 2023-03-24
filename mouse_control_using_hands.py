3cce6871606850385400241baae24f6ed41f9dd1
shkim1224 <shkim@kunsan.ac.kr>
2023-03-24 15:33:53 +0900

    3rd

--- mouse_control_using_hands.py
+++ mouse_control_using_hands.py
@@ -3,19 +3,43 @@ import mediapipe
 import pyautogui
 capture_hands = mediapipe.solutions.hands.Hands() # 
 drawing_option = mediapipe.solutions.drawing_utils  # 너무 기니까 drawing_option 으로 간략화 시킴
-
+screen_width, screen_height = pyautogui.size()
 camera = cv2.VideoCapture(0)
+x1 = y1 = x2 = y2 =0
 while True:
     _,image = camera.read()
+    image_height, image_width, _ = image.shape
     image = cv2.flip(image,1)
     rgb_image = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
     output_hands = capture_hands.process(rgb_image)
-    cv2.imshow("Hand movement video caputre", image)
+    # cv2.imshow("Hand movement video caputre", image)
     all_hands = output_hands.multi_hand_landmarks  # capture_hands 객체에 있는 데이터(multi_hand_landmarks)
     if all_hands:   # output_hands 객체에서 검출된 손이 있다면 
         for hand in all_hands:    # 각각을 찾아서 hand 라는 변수에 저장
             drawing_option.draw_landmarks(image, hand)
-        
+            one_hand_landmarks = hand.landmark
+            for id, lm in enumerate(one_hand_landmarks):
+                x=int(lm.x *image_width)
+                y=int(lm.y *image_height)
+                # print(x,y)
+                if id == 8:
+                    mouse_x = int(screen_width / image_width *x)
+                    mouse_y = int(screen_height / image_height *y)
+                    cv2.circle(image,(x,y),10,(0,255,255))
+                    pyautogui.moveTo(mouse_x,mouse_y)
+                    x1 = x
+                    y1 = y
+                if id == 4:
+                    x2 = x
+                    y2 = y
+                    cv2.circle(image,(x,y),10,(255,0,255))
+        dist = y2 -y1
+        print(dist)
+        if (dist <30):
+            pyautogui.click()
+            
+         
+    cv2.imshow("Hand movement video capture",image)    
     key = cv2.waitKey(100)
     if key == 27:
         break
